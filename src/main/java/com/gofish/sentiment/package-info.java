ff7ca906456a9bc2b828f8459cb1a62832cf1b35
Luke Herron <lukeherron@gmail.com>
2017-01-27 12:32:06 +0000

    WIP: sentimentservice verticle placeholder removed: previous single-project/module files/folders
    moved gradle wrapper task outside of allprojects() method, to avoid
    generating gradle wrapper for every submodule.

--- src/main/java/com/gofish/sentiment/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * @author Luke Herron
- */
-@ModuleGen(groupPackage = "com.gofish.sentiment", name = "sentiment-root-module")
-package com.gofish.sentiment;
-
-import io.vertx.codegen.annotations.ModuleGen;
