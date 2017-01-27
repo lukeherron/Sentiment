b64d48b468dd6cbe344e4ecb204bdf2c51b9e52c
Luke Herron <lukeherron@gmail.com>
2017-01-27 12:34:03 +0000

    Add package-info file, annotated to support vertx code generation, for sentiment-service module

---
+++ sentiment-service/src/main/java/com/gofish/sentiment/sentimentservice/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * @author Luke Herron
+ */
+@ModuleGen(groupPackage = "com.gofish.sentiment.sentimentservice", name = "sentiment-service")
+package com.gofish.sentiment.sentimentservice;
+
+import io.vertx.codegen.annotations.ModuleGen;
