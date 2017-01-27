341ec9fb27283050c86263d11f73b16b6271b01f
Luke Herron <lukeherron@gmail.com>
2017-01-27 10:46:46 +0000

    WIP: Added news-analyser verticle placeholder

---
+++ news-analyser/src/main/java/com/gofish/sentiment/newsanalyser/NewsAnalyserVerticle.java
@@ -0,0 +1,16 @@
+package com.gofish.sentiment.newsanalyser;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+
+/**
+ * @author Luke Herron
+ */
+public class NewsAnalyserVerticle extends AbstractVerticle {
+
+    @Override
+    public void start(Future<Void> startFuture) throws Exception {
+        // WIP: Verticle placeholder
+        super.start(startFuture);
+    }
+}
