ba10ca84f1468d3231f33dda02da9c9f6d5f421b
Luke Herron <lukeherron@gmail.com>
2017-01-27 10:59:49 +0000

    WIP: news-crawler verticle placeholder

---
+++ news-crawler/src/main/java/com/gofish/sentiment/newscrawler/NewsCrawlerVerticle.java
@@ -0,0 +1,16 @@
+package com.gofish.sentiment.newscrawler;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+
+/**
+ * @author Luke Herron
+ */
+public class NewsCrawlerVerticle extends AbstractVerticle {
+
+    @Override
+    public void start(Future<Void> startFuture) throws Exception {
+        // WIP: news-crawler verticle placeholder
+        super.start(startFuture);
+    }
+}
