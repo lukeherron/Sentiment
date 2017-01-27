36a996acaf82d11b92227517ca5cb0322112bf8f
Luke Herron <lukeherron@gmail.com>
2017-01-27 11:06:55 +0000

    WIP: news-linker verticle placeholder

---
+++ news-linker/src/main/java/com/gofish/sentiment/newslinker/NewsLinkerVerticle.java
@@ -0,0 +1,16 @@
+package com.gofish.sentiment.newslinker;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+
+/**
+ * @author Luke Herron
+ */
+public class NewsLinkerVerticle extends AbstractVerticle {
+
+    @Override
+    public void start(Future<Void> startFuture) throws Exception {
+        // WIP: news-linker verticle placeholder
+        super.start(startFuture);
+    }
+}
