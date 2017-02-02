898a1fe19ecc42979da750e456d90dc521ae6713
Luke Herron <lukeherron@gmail.com>
2017-02-02 22:26:03 +0000

    Unit test added: test StorageWorker verticle deploys

--- storage/src/test/java/com/gofish/sentiment/storage/StorageWorkerTest.java
+++ storage/src/test/java/com/gofish/sentiment/storage/StorageWorkerTest.java
@@ -1,6 +1,38 @@
 package com.gofish.sentiment.storage;
 
+import io.vertx.core.Vertx;
+import io.vertx.ext.mongo.MongoClient;
+import io.vertx.ext.unit.TestContext;
+import io.vertx.ext.unit.junit.RunTestOnContext;
+import io.vertx.ext.unit.junit.VertxUnitRunner;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.mockito.Mockito.mock;
+
 /**
  * @author Luke Herron
  */
-public class StorageWorkerTest {}
+@RunWith(VertxUnitRunner.class)
+public class StorageWorkerTest {
+
+    @Rule
+    public final RunTestOnContext vertxRule = new RunTestOnContext();
+
+    private Vertx vertx;
+    private MongoClient mongo;
+    private StorageVerticle storageVerticle;
+
+    @Before
+    public void setUp(TestContext context) {
+        vertx = vertxRule.vertx();
+        mongo = mock(MongoClient.class);
+    }
+
+    @Test
+    public void testVerticleDeploysSuccessfully(TestContext context) {
+        vertx.deployVerticle(StorageWorker.class.getName(), context.asyncAssertSuccess(context::assertNotNull));
+    }
+}
