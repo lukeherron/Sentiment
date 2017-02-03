7e88dbb66a43dc7bd556881a416aafde58ba6b1c
Luke Herron <lukeherron@gmail.com>
2017-02-03 20:40:05 +0000

    Updated StorageVerticle integration test to clear embedded mongo db on each run.
    Minor updates to Storage module gradle build for launching/stopping
    embedded mongo db on test runs.

--- storage/src/testIT/java/com/gofish/sentiment/storage/StorageVerticleIT.java
+++ storage/src/testIT/java/com/gofish/sentiment/storage/StorageVerticleIT.java
@@ -2,6 +2,7 @@ package com.gofish.sentiment.storage;
 
 import io.vertx.core.Vertx;
 import io.vertx.core.json.JsonObject;
+import io.vertx.ext.mongo.MongoClient;
 import io.vertx.ext.unit.TestContext;
 import io.vertx.ext.unit.junit.VertxUnitRunner;
 import io.vertx.servicediscovery.ServiceDiscovery;
@@ -27,6 +28,13 @@ public class StorageVerticleIT {
         vertx = Vertx.vertx();
         vertx.exceptionHandler(context.exceptionHandler());
         serviceDiscovery = ServiceDiscovery.create(vertx);
+
+        JsonObject command = new JsonObject()
+                .put("dropDatabase", 1);
+
+        // We'll start with a clean DB on each run
+        MongoClient.createNonShared(vertx, new JsonObject())
+                .runCommand("dropDatabase", command, context.asyncAssertSuccess());
 
         vertx.eventBus().<JsonObject>consumer("vertx.discovery.announce", messageHandler -> {
             JsonObject announce = messageHandler.body();
@@ -69,7 +77,7 @@ public class StorageVerticleIT {
     public void testStorageServiceCanGetCollections(TestContext context) {
         storageService.getCollections(context.asyncAssertSuccess(context::assertNotNull));
 
-        storageService.createCollection("oneCollection", context.asyncAssertSuccess(result ->
+        storageService.createCollection("testCollection", context.asyncAssertSuccess(result ->
                 storageService.getCollections(context.asyncAssertSuccess(collections ->
                         context.assertTrue(collections.size() > 0)))));
     }
@@ -80,5 +88,28 @@ public class StorageVerticleIT {
         context.assertNotNull(service);
 
         service.getCollections(context.asyncAssertSuccess(context::assertNotNull));
+    }
+
+    @Test
+    public void testStorageServiceCanCheckCollectionExists(TestContext context) {
+        storageService.hasCollection("collectionThatDoesNotExist", context.asyncAssertSuccess(context::assertFalse));
+        storageService.hasCollection(null, context.asyncAssertSuccess(context::assertFalse));
+
+        storageService.createCollection("testCollection", context.asyncAssertSuccess(result -> {
+            storageService.hasCollection("testCollection", context.asyncAssertSuccess(context::assertTrue));
+        }));
+    }
+
+    @Test
+    public void testStorageServiceProxyCanCheckCollectionExists(TestContext context) {
+        StorageService service = StorageService.createProxy(vertx, StorageService.ADDRESS);
+        context.assertNotNull(service);
+
+        service.hasCollection("proxyCollectionThatDoesNotExist", context.asyncAssertSuccess(context::assertFalse));
+        service.hasCollection(null, context.asyncAssertSuccess(context::assertFalse));
+
+        service.createCollection("testCollection", context.asyncAssertSuccess(result -> {
+            service.hasCollection("testCollection", context.asyncAssertSuccess(context::assertTrue));
+        }));
     }
 }
