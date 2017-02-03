40bf95b7389e6c4e0f1a88aa3759d5b26d2ef960
Luke Herron <lukeherron@gmail.com>
2017-02-03 16:12:34 +0000

    Unit test added: test StorageService saveArticles() method

--- storage/src/test/java/com/gofish/sentiment/storage/StorageServiceTest.java
+++ storage/src/test/java/com/gofish/sentiment/storage/StorageServiceTest.java
@@ -135,6 +135,26 @@ public class StorageServiceTest {
                 context.assertEquals("failed test", result.getMessage())));
     }
 
+    @Test
+    @SuppressWarnings("unchecked")
+    public void testSaveArticlesSucceeds(TestContext context) {
+        Message<Object> message = mock(Message.class);
+        prepareSuccessScenario(message);
+
+        when(message.body()).thenReturn(new JsonObject().put("result", "success"));
+
+        storageService.saveArticles("testCollection", new JsonArray(), context.asyncAssertSuccess(result ->
+                context.assertEquals("success", result.getString("result"))));
+    }
+
+    @Test
+    public void testSaveArticlesFails(TestContext context) {
+        prepareFailureScenario();
+
+        storageService.saveArticles("testCollection", new JsonArray(), context.asyncAssertFailure(result ->
+                context.assertEquals("failed test", result.getMessage())));
+    }
+
     private void prepareSuccessScenario(Message<Object> message) {
         Future<Message<Object>> future = Future.succeededFuture(message);
         setMessageResponse(future);
