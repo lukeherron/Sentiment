8c6bf5d879e6eae50ccd9a3fd870f516143d1fbe
Luke Herron <lukeherron@gmail.com>
2017-02-03 17:24:20 +0000

    Initial integration test created for StorageVerticle. Tests that proxy for StorageService api is successfully published/ unpublished to cluster.

---
+++ storage/src/testIT/java/com/gofish/sentiment/storage/StorageVerticleIT.java
@@ -0,0 +1,56 @@
+package com.gofish.sentiment.storage;
+
+import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.unit.TestContext;
+import io.vertx.ext.unit.junit.VertxUnitRunner;
+import io.vertx.servicediscovery.ServiceDiscovery;
+import io.vertx.servicediscovery.types.EventBusService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Luke Herron
+ */
+@RunWith(VertxUnitRunner.class)
+public class StorageVerticleIT {
+
+    private Vertx vertx;
+    private ServiceDiscovery serviceDiscovery;
+    private StorageService storageService;
+
+    private String serviceDiscoveryStatus = "";
+
+    @Before
+    public void setUp(TestContext context) {
+        vertx = Vertx.vertx();
+        vertx.exceptionHandler(context.exceptionHandler());
+        serviceDiscovery = ServiceDiscovery.create(vertx);
+
+        vertx.eventBus().<JsonObject>consumer("vertx.discovery.announce", messageHandler -> {
+            JsonObject announce = messageHandler.body();
+            serviceDiscoveryStatus = announce.getString("status");
+        });
+
+        vertx.deployVerticle(StorageVerticle.class.getName(), context.asyncAssertSuccess(result -> {
+            EventBusService.getProxy(serviceDiscovery, StorageService.class, context.asyncAssertSuccess(storageService -> {
+                this.storageService = storageService;
+            }));
+        }));
+    }
+
+    @Test
+    public void testStorageServicePublishedStatus(TestContext context) {
+        context.assertEquals(serviceDiscoveryStatus, "UP", "Service Discovery Status returned '" +
+                serviceDiscoveryStatus + "'. Expected 'UP'");
+    }
+
+    @Test
+    public void testStorageServiceUnpublishedStatus(TestContext context) {
+        vertx.close(completionHandler -> {
+            context.assertEquals(serviceDiscoveryStatus, "DOWN", "Service Discovery Status returned '" +
+                    serviceDiscoveryStatus + "'. Expected 'DOWN'");
+        });
+    }
+}
