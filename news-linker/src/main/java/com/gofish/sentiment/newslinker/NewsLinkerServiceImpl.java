edf334ef05b22eae2ae3dfc77420d1f2a98c4f59
Luke Herron <lukeherron@gmail.com>
2017-02-06 15:59:55 +0000

    Implemented NewsLinkerServiceImpl + NewsLinkerService tests. Updates to news-linker modules gradle build to support tests.

--- news-linker/src/main/java/com/gofish/sentiment/newslinker/NewsLinkerServiceImpl.java
+++ news-linker/src/main/java/com/gofish/sentiment/newslinker/NewsLinkerServiceImpl.java
@@ -1,21 +1,50 @@
 package com.gofish.sentiment.newslinker;
 
-import io.vertx.core.AsyncResult;
-import io.vertx.core.Handler;
-import io.vertx.core.Vertx;
+import io.vertx.core.*;
+import io.vertx.core.eventbus.Message;
 import io.vertx.core.json.JsonObject;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
 
 /**
  * @author Luke Herron
  */
 public class NewsLinkerServiceImpl implements NewsLinkerService {
 
-    public NewsLinkerServiceImpl(Vertx vertx, JsonObject config) {
+    private static final Logger LOG = LoggerFactory.getLogger(NewsLinkerServiceImpl.class);
 
+    private final Vertx vertx;
+    private final DeploymentOptions workerOptions;
+
+    public NewsLinkerServiceImpl(Vertx vertx, JsonObject config) {
+        this.vertx = vertx;
+        this.workerOptions = new DeploymentOptions().setConfig(config).setWorker(true);
     }
 
     @Override
-    public void linkEntities(JsonObject document, Handler<AsyncResult<JsonObject>> resultHandler) {
+    public void linkEntities(JsonObject article, Handler<AsyncResult<JsonObject>> resultHandler) {
+        LOG.info("Starting entity linking");
 
+        vertx.deployVerticle(NewsLinkerWorker.class.getName(), workerOptions, completionHandler -> {
+            if (completionHandler.succeeded()) {
+                JsonObject message = new JsonObject().put("article", article);
+                vertx.eventBus().send(NewsLinkerWorker.ADDRESS, message, handleReply(resultHandler));
+            }
+            else {
+                resultHandler.handle(Future.failedFuture(completionHandler.cause()));
+            }
+        });
+    }
+
+    private Handler<AsyncResult<Message<JsonObject>>> handleReply(Handler<AsyncResult<JsonObject>> resultHandler) {
+        return replyHandler -> {
+            if (replyHandler.succeeded()) {
+                JsonObject result = replyHandler.result().body();
+                resultHandler.handle(Future.succeededFuture(result));
+            }
+            else {
+                resultHandler.handle(Future.failedFuture(replyHandler.cause()));
+            }
+        };
     }
 }
