dc0f11a45159750fd08b3e409eebe63003c0e27a
Luke Herron <lukeherron@gmail.com>
2017-04-22 17:00:25 +0100

    Update NewsCrawlerService implementation to meet changes to implemented interface Adds getTimeout to retrieve any timeout delays before making service calls, and setTimeout as the mutator

--- news-crawler/src/main/java/com/gofish/sentiment/newscrawler/NewsCrawlerServiceImpl.java
+++ news-crawler/src/main/java/com/gofish/sentiment/newscrawler/NewsCrawlerServiceImpl.java
@@ -1,5 +1,7 @@
 package com.gofish.sentiment.newscrawler;
 
+import io.vertx.circuitbreaker.CircuitBreaker;
+import io.vertx.circuitbreaker.CircuitBreakerOptions;
 import io.vertx.core.AsyncResult;
 import io.vertx.core.Future;
 import io.vertx.core.Handler;
@@ -16,6 +18,8 @@ import io.vertx.rx.java.RxHelper;
 import io.vertx.rx.java.SingleOnSubscribeAdapter;
 import rx.Single;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 /**
  * @author Luke Herron
  */
@@ -28,12 +32,16 @@ public class NewsCrawlerServiceImpl implements NewsCrawlerService {
 
     private final WebClient webClient;
     private final HttpRequest<JsonObject> request;
+    private final CircuitBreaker breaker;
     private final String apiKey;
     private final String baseUrl;
     private final String freshness;
     private final String urlPath;
     private final Integer port;
     private final Integer resultCount;
+
+    private final AtomicLong timeoutTimeStamp;
+    private final AtomicLong timeoutDelay;
 
     public NewsCrawlerServiceImpl(Vertx vertx, JsonObject config) {
         JsonObject apiConfig = config.getJsonObject("api");
@@ -46,6 +54,11 @@ public class NewsCrawlerServiceImpl implements NewsCrawlerService {
 
         webClient = WebClient.create(vertx, getWebClientOptions());
         request = getHttpRequest();
+        breaker = CircuitBreaker.create("newscrawler-circuit-breaker", vertx,
+                new CircuitBreakerOptions().setMaxRetries(5).setMaxFailures(5).setTimeout(30000).setResetTimeout(30000));
+
+        timeoutTimeStamp = new AtomicLong(0);
+        timeoutDelay = new AtomicLong(0);
     }
 
     @Override
@@ -63,11 +76,37 @@ public class NewsCrawlerServiceImpl implements NewsCrawlerService {
                 .subscribe(RxHelper.toSubscriber(resultHandler));
     }
 
+    /**
+     * Takes the query string and sends it to the Bing News Search API. This API performs a news search, returning
+     * articles which are related to the supplied query.
+     * @param query String query to search the news for
+     * @return Single which emits the result of the HttpResponse
+     */
     private Single<HttpResponse<JsonObject>> rxCrawlQuery(String query) {
 
-        return Single.create(new SingleOnSubscribeAdapter<>(fut -> request.setQueryParam("q", query).send(fut)));
+        return Single.create(new SingleOnSubscribeAdapter<>(fut -> {
+            breaker.<HttpResponse<JsonObject>>execute(future -> {
+                request.setQueryParam("q", query).send(response -> {
+                    if (response.failed()) {
+                        LOG.error(response.cause().getMessage(), response.cause());
+                        future.fail(response.cause());
+                    }
+
+                    HttpResponse<JsonObject> result = response.result();
+                    if (result.statusCode() != 200 && result.statusCode() != 429) {
+                        future.fail(result.body() == null ? result.statusMessage() : result.body().encode());
+                    } else {
+                        future.complete(result);
+                    }
+                });
+            }).setHandler(fut);
+        }));
     }
 
+    /**
+     * Retrieves the HttpRequest, configured for access to the Microsoft Cognitive Services API
+     * @return HTTP client request object
+     */
     private HttpRequest<JsonObject> getHttpRequest() {
 
         return webClient.get(port, baseUrl, urlPath)
@@ -92,5 +131,17 @@ public class NewsCrawlerServiceImpl implements NewsCrawlerService {
                 .setIdleTimeout(0)
                 .setSsl(!baseUrl.equalsIgnoreCase("localhost"))
                 .setKeepAlive(true);
+    }
+
+    @Override
+    public void getTimeout(Handler<AsyncResult<Long>> timeoutHandler) {
+        final long delay = timeoutDelay.get() - (System.currentTimeMillis() - timeoutTimeStamp.get());
+        timeoutHandler.handle(Future.succeededFuture(delay));
+    }
+
+    @Override
+    public void setTimeout(Long delay) {
+        timeoutTimeStamp.set(System.currentTimeMillis());
+        timeoutDelay.set(delay);
     }
 }
