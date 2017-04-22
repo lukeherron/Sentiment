42be37bd1aef2640ac8e7a062f4ffc87ce40dfa5
Luke Herron <lukeherron@gmail.com>
2017-04-22 17:07:00 +0100

    Wrapped API call in circuit-breaker Update NewsAnalyserService implementation to meet changes to implemented interface Adds getTimeout to retrieve any timeout delays before making service calls, and setTimeout as the mutator

--- news-analyser/src/main/java/com/gofish/sentiment/newsanalyser/NewsAnalyserServiceImpl.java
+++ news-analyser/src/main/java/com/gofish/sentiment/newsanalyser/NewsAnalyserServiceImpl.java
@@ -1,5 +1,7 @@
 package com.gofish.sentiment.newsanalyser;
 
+import io.vertx.circuitbreaker.CircuitBreaker;
+import io.vertx.circuitbreaker.CircuitBreakerOptions;
 import io.vertx.core.AsyncResult;
 import io.vertx.core.Future;
 import io.vertx.core.Handler;
@@ -19,6 +21,7 @@ import rx.Single;
 
 import java.util.Optional;
 import java.util.UUID;
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * @author Luke Herron
@@ -27,14 +30,20 @@ public class NewsAnalyserServiceImpl implements NewsAnalyserService {
 
     private static final Logger LOG = LoggerFactory.getLogger(NewsAnalyserServiceImpl.class);
 
+    private final Vertx vertx;
     private final WebClient webClient;
     private final HttpRequest<JsonObject> request;
+    private final CircuitBreaker breaker;
     private final String apiKey;
     private final String baseUrl;
     private final String urlPath;
     private final Integer port;
 
+    private final AtomicLong timeoutTimeStamp;
+    private final AtomicLong timeoutDelay;
+
     public NewsAnalyserServiceImpl(Vertx vertx, JsonObject config) {
+        this.vertx = vertx;
         JsonObject apiConfig = config.getJsonObject("api");
         apiKey = apiConfig.getString("key", "");
         baseUrl = apiConfig.getString("base.url", "");
@@ -43,6 +52,11 @@ public class NewsAnalyserServiceImpl implements NewsAnalyserService {
 
         webClient = WebClient.create(vertx, getWebClientOptions());
         request = getHttpRequest();
+        breaker = CircuitBreaker.create("newsanalyser-circuit-breaker", vertx,
+                new CircuitBreakerOptions().setMaxRetries(5).setMaxFailures(5).setTimeout(30000).setResetTimeout(30000));
+
+        timeoutTimeStamp = new AtomicLong(0);
+        timeoutDelay = new AtomicLong(0);
     }
 
     @Override
@@ -61,36 +75,70 @@ public class NewsAnalyserServiceImpl implements NewsAnalyserService {
         else if (articleDescription == null) {
             resultHandler.handle(Future.failedFuture("Invalid article lead paragraph supplied"));
         }
+        else {
+            final String text = String.join(". ", articleName, articleDescription);
+            final JsonObject requestData = new JsonObject().put("documents", new JsonArray()
+                    .add(new JsonObject()
+                            .put("language", "en")
+                            .put("id", UUID.randomUUID().toString())
+                            .put("text", text)));
 
-        final String text = String.join(". ", articleName, articleDescription);
-        final JsonObject requestData = new JsonObject().put("documents", new JsonArray()
-                .add(new JsonObject()
-                        .put("language", "en")
-                        .put("id", UUID.randomUUID().toString())
-                        .put("text", text)));
-
-        rxAnalyseSentiment(requestData)
-                .map(HttpResponse::body)
-                .flatMap(body -> rxAddSentimentResults(article, body))
-                .subscribe(RxHelper.toSubscriber(resultHandler));
+            rxAnalyseSentiment(requestData)
+                    .map(HttpResponse::body)
+                    .flatMap(body -> rxAddSentimentResults(article, body))
+                    .subscribe(RxHelper.toSubscriber(resultHandler));
+        }
     }
 
+    /**
+     * Takes request data and sends it to the Microsoft Cognitive Services API. This API processes the text submitted in
+     * the request and returns a sentiment analysis score as part of the response
+     * @param requestData JsonObject representing the request data to send
+     * @return Single which emits the result of the HttpResponse
+     */
     private Single<HttpResponse<JsonObject>> rxAnalyseSentiment(JsonObject requestData) {
 
-        return Single.create(new SingleOnSubscribeAdapter<>(fut ->
-                request.sendJson(requestData, fut)));
+        return Single.create(new SingleOnSubscribeAdapter<HttpResponse<JsonObject>>(fut -> {
+            breaker.<HttpResponse<JsonObject>>execute(future -> {
+                request.sendJson(requestData, response -> {
+                    if (response.failed()) {
+                        LOG.error(response.cause().getMessage(), response.cause());
+                        future.fail(response.cause());
+                    }
+
+                    HttpResponse<JsonObject> result = response.result();
+                    if (result.statusCode() != 200 && result.statusCode() != 429) {
+                        future.fail(result.body() == null ? result.statusMessage() : result.body().encode());
+                    } else {
+                        future.complete(result);
+                    }
+                });
+            }).setHandler(fut);
+        }));
     }
 
+    /**
+     * Updates a supplied news article with the supplied sentiment analysis response
+     * @param article JsonObject which represents the news article to be updated with the sentiment results
+     * @param analysisResponse JsonObject which holds the sentiment analysis results
+     * @return Single which emits the news article which has been updated with the sentiment results
+     */
     private Single<JsonObject> rxAddSentimentResults(JsonObject article, JsonObject analysisResponse) {
         JsonArray documents = Optional.ofNullable(analysisResponse.getJsonArray("documents"))
-                .orElseThrow(() -> new RuntimeException(analysisResponse.encode()));
+                .orElseThrow(() -> new RuntimeException(analysisResponse.containsKey("error") ?
+                        analysisResponse.encode() : new JsonObject().put("error", analysisResponse).encode()));
 
         article.put("sentiment", documents.getJsonObject(0));
 
         return Single.just(article);
     }
 
+    /**
+     * Retrieves the HttpRequest, configured for access to the Microsoft Cognitive Services API
+     * @return HTTP client request object
+     */
     private HttpRequest<JsonObject> getHttpRequest() {
+
         return webClient.post(port, baseUrl, urlPath)
                 .putHeader("Ocp-Apim-Subscription-Key", apiKey)
                 .as(BodyCodec.jsonObject());
@@ -103,11 +151,24 @@ public class NewsAnalyserServiceImpl implements NewsAnalyserService {
      * @return HttpClientOptions object to configure this verticles HttpClient
      */
     private WebClientOptions getWebClientOptions() {
+
         return new WebClientOptions()
                 .setPipelining(true)
                 .setPipeliningLimit(10)
                 .setIdleTimeout(0)
                 .setSsl(!baseUrl.equalsIgnoreCase("localhost"))
                 .setKeepAlive(true);
+    }
+
+    @Override
+    public void getTimeout(Handler<AsyncResult<Long>> timeoutHandler) {
+        final long delay = timeoutDelay.get() - (System.currentTimeMillis() - timeoutTimeStamp.get());
+        timeoutHandler.handle(Future.succeededFuture(delay));
+    }
+
+    @Override
+    public void setTimeout(Long delay) {
+        timeoutTimeStamp.set(System.currentTimeMillis());
+        timeoutDelay.set(delay);
     }
 }
